20fb9b7217d4f4090fa0983b9c496757eb2685e0
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-06-16 21:59:51 +0800

    Attempt to rewind by saving vector

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -10,46 +10,53 @@ var motion = Vector2()
 var friction = false
 var doubleJumped = false
 
-func _physics_process(delta):
-	# Gravity
-	motion.y += GRAVITY
-	
-	# Controls
-	if Input.is_action_pressed("ui_right"):
-		motion.x = min(motion.x + ACC , SPEED_UPPER)
-		
-		$Sprite.flip_h = false
-		$Sprite.animation = "run"
-		
-	elif Input.is_action_pressed("ui_left"):
-		
-		motion.x = max(motion.x - ACC , -SPEED_UPPER)
-		
-		$Sprite.flip_h = true
-		$Sprite.animation = "run" 
-	else:
-		motion.x = 0 
-		
-		$Sprite.animation = "idle"
-		
-		friction = true
+var motion_hist = Array()
 
-	if is_on_floor():
-		doubleJumped = false 
-		if friction == true:
-			
-			motion.x = lerp(motion.x, 0, 0.2)
-		if Input.is_action_just_pressed("ui_up"): 
-		
-			motion.y = JUMP_HEIGHT
-	elif Input.is_action_just_pressed("ui_up") && !doubleJumped:
-			doubleJumped = true
-			motion.y = JUMP_HEIGHT/ 2 # Double jump
+func _physics_process(delta):
+	print(motion_hist.size())
+	if Input.is_action_pressed("ui_down") && motion_hist.size() > 0:
+		$Sprite.animation = "fall"
+		motion = motion_hist.pop_back() * Vector2(-1,-1)
 	else:
-		motion.x = lerp(motion.x, 0, 0.1)
+		# Gravity
+		motion.y += GRAVITY
+
+		# if Input.is_action_just_released("ui_down"):
+			# motion_hist.clear()
+		# Controls
+		if Input.is_action_pressed("ui_right"):
+			motion.x = min(motion.x + ACC , SPEED_UPPER)
+			motion_hist.append(motion)
+			$Sprite.flip_h = false
+			$Sprite.animation = "run"
+			
+		elif Input.is_action_pressed("ui_left"):
+			motion.x = max(motion.x - ACC , -SPEED_UPPER)
+			motion_hist.append(motion)
+			$Sprite.flip_h = true
+			$Sprite.animation = "run" 
+		else:
+			motion.x = 0 
+			$Sprite.animation = "idle"
+			friction = true
+
+		if is_on_floor():
+			doubleJumped = false 
+			if friction == true:
+				motion.x = lerp(motion.x, 0, 0.2)
+			if Input.is_action_just_pressed("ui_up"): 
+				motion.y = JUMP_HEIGHT
+				motion_hist.append(motion)
+
+
+		elif Input.is_action_just_pressed("ui_up") && !doubleJumped:
+				doubleJumped = true
+				motion.y = JUMP_HEIGHT/ 2 # Double jump
+				motion_hist.append(motion)
+
+		else:
+			motion.x = lerp(motion.x, 0, 0.1)
+			$Sprite.animation = "jump" if motion.y < 0 else "fall"
 		
-		$Sprite.animation = "jump" if motion.y < 0 else "fall"
-	
 	motion = move_and_slide(motion, UP)
-	
 	pass
