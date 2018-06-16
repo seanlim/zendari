817296094d45ed635a6a4af970395a4718c1f9a2
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-06-16 17:02:03 +0800

    Add sky

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 const GRAVITY = 20
 const ACC = 40
 const SPEED_UPPER = 250
-const JUMP_HEIGHT = -500
+const JUMP_HEIGHT = -400
 const UP = Vector2(0,-1)
-var motion = Vector2()
 
+var motion = Vector2()
 var friction = false
+var doubleJumped = false
 
 func _physics_process(delta):
 	# Gravity
@@ -32,12 +33,17 @@ func _physics_process(delta):
 		$Sprite.animation = "idle"
 		
 		friction = true
+
 	if is_on_floor():
+		doubleJumped = false 
 		if friction == true:
 			
 			motion.x = lerp(motion.x, 0, 0.2)
 		if Input.is_action_just_pressed("ui_up"): 
 		
+			motion.y = JUMP_HEIGHT
+	elif Input.is_action_just_pressed("ui_up") && !doubleJumped:
+			doubleJumped = true
 			motion.y = JUMP_HEIGHT
 	else:
 		motion.x = lerp(motion.x, 0, 0.1)
