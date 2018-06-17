ab2510a0226b2d4f887ca8b015d5276fbb8fecd3
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-06-17 16:37:00 +0800

    Setup portal

--- Portal.gd
+++ Portal.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+export (String, FILE, "*.tscn") var Next_Scene
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
 			print("Portal warp")
+			get_tree().change_scene(Next_Scene)
