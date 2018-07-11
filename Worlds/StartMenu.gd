ee90d62ec4abba0ff822768379b9c4f8f80e0e86
JoshuaOng <joshua.ong2018@gmail.com>
2018-07-11 15:07:29 +0800

    Add start menu

---
+++ Worlds/StartMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+var anim_state = null
+	
+func _on_Play_pressed():
+	get_node("CenterContainer/VBoxContainer/CenterContainer/VBoxContainer/Play").set_disabled(true)
+	$Sprite/StartAnim.play("Start_Run")
+	$Sprite.animation = "Run"
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func _on_StartAnim_animation_finished(Start_Run):
+
+	get_tree().change_scene("res://Worlds/Sandbox.tscn")
