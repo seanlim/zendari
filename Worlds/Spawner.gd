a4fd0163f310d612e837e384f8b4b309ca9cc46f
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-07-31 02:07:33 +0800

    Add spawner animation

--- Worlds/Spawner.gd
+++ Worlds/Spawner.gd
@@ -14,6 +14,7 @@ func _process(delta):
 		timer += delta
 		if (timer > rate):
 			print('Will spawn')
+			$AnimationPlayer.current_animation = 'spawn'
 			timer -= rate
 			var addMonster = monster.instance()
 			addMonster.GRAVITY = 180
