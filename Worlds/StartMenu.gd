ea223414266ec0e8bdbb250f3187579102042c36
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-08-07 16:43:32 +0800

    Add better soundtrack

--- Worlds/StartMenu.gd
+++ Worlds/StartMenu.gd
@@ -3,12 +3,13 @@ extends Control
 var timer = null
 
 func _ready():
+	Engine.time_scale = 0.05
 	$Player.GRAVITY = 5.0
 	$Player.enabled = false
 	timer = Timer.new()
 	timer.set_one_shot(true)
 	timer.set_timer_process_mode(Timer.TIMER_PROCESS_IDLE)
-	timer.set_wait_time(5)
+	timer.set_wait_time(1)
 	timer.connect("timeout", self, "transition")
 	timer.start()
 	add_child(timer)
@@ -16,6 +17,7 @@ func _ready():
 func transition():
 	remove_child(timer)
 	timer.queue_free()
+	Engine.time_scale = 1.0
 	get_tree().change_scene("res://Worlds/Tutorial.tscn")
 
 
