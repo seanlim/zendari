d1bbba950416ca9e3e890821dd4a8d800784cdb4
Sean Lim <lim_kai_xiang_sean@s2013.sst.edu.sg>
2018-08-08 14:48:54 +0800

    Update tutorial level 2

--- Worlds/LegitTutorial2.gd
+++ Worlds/LegitTutorial2.gd
@@ -33,8 +33,6 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	track($Player)
-	track($Platform)
-	track($Platform8)
 	track($Platform9)
 	track($Platform10)
 	track($Platform6)
